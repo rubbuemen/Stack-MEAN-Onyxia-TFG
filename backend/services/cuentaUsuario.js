1a7ec3bfd403245a5b995cbf4916913fe9198772
rubbuemen <ruben_077@msn.com>
2020-06-18 19:03:16 +0200

    Refactorización de la estructura (rafactor) Se ha refactorizado el nombre de los ficheros para diferenciarlos mejor

--- backend/services/cuentaUsuario.js
+++
@@ -1,9 +0,0 @@
-const errorLanzado = require('../util/error');
-const { CuentaUsuario } = require('../models/cuentaUsuario');
-
-exports.getUsuarioLogeado = async (usuario) => {
-  const resultado = await CuentaUsuario.findOne({ usuario: usuario });
-  if (!resultado) throw errorLanzado(404, 'El usuario introducido no existe');
-  if (!resultado.estado) throw errorLanzado(403, 'El usuario introducido está baneado');
-  return resultado;
-};
--- backend/util/error.js
+++
@@ -1,8 +0,0 @@
-function errorLanzado(codigo, mensaje) {
-  const error = new Error();
-  error.message = mensaje;
-  error.status = codigo;
-  return error;
-}
-
-module.exports = errorLanzado;
