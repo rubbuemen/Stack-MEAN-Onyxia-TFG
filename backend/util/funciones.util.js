0d89d1c4ded936d725452129b95f716f150addbc
rubbuemen <ruben_077@msn.com>
2020-06-30 13:25:43 +0200

    Funciones auxiliares (fix) Definidas las exportaciones de las funciones auxiliares de otra forma

--- backend/util/funciones.util.js
+++ backend/util/funciones.util.js
@@ -1,15 +1,12 @@
-var fs = require('fs');
+const fs = require('fs');
 
-function convertirImagenABase64(file) {
+exports.convertirImagenABase64 = function convertirImagenABase64(file) {
   const imagen = fs.readFileSync(file.path);
   return imagen;
-}
+};
 
-async function asyncForEach(array, callback) {
+exports.asyncForEach = async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
-}
-
-module.exports = convertirImagenABase64;
-module.exports = asyncForEach;
+};
